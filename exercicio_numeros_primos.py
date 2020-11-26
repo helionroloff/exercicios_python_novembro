842c5d8ac59c00b4a7e50af5785da96becaea4c1
helionroloff <helionroloff@gmail.com>
2020-11-26 11:25:53 -0300

    exercicios revisão python

--- exercicio_numeros_primos.py
+++ exercicio_numeros_primos.py
@@ -1,16 +1,26 @@
 #Faça um programa que gera uma lista dos números primos existentes entre 1 e 
 # um número inteiro informado pelo usuário. 
+def numeros_primos(intervalo):
+    numeros_primos = []
+    for i in range(1,numero + 1):
+        cont = 0
+        for x in range(1, i + 1):
+            if i%x == 0:
+                cont = cont+1
+        if cont == 2:
+            numeros_primos.append(x)
+    return numeros_primos
 
-numero = int(input('informe o número desejado: '))
-numeros_primos = []
-for i in range(1,numero + 1):
-    cont = 0
-    for x in range(1, i + 1):
-        if i%x == 0:
-            cont = cont+1
-    if cont == 2:
-        numeros_primos.append(x)
 
-print(numeros_primos)
+if __name__ == "__main__":
+    while True:
+        numero = int(input('números primos no intervalo de 1 a: '))
+        resultado = numeros_primos(numero)
+        print(*resultado)
+        nova_consulta = input('DESEJA FAZER OUTRA CONSULTA? S - N').upper()
+        if nova_consulta == 'N':
+            print('OBRIGADO')
+            break
+    
 
 
